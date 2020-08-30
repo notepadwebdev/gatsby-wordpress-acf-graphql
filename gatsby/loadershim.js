9182c576f490b80fc7ce358581037643619e95a5
Stephen Griffin <griffleriff@gmail.com>
2020-08-30 16:43:33 +0100

    add comment explaining prop type errors / jest fail

--- gatsby/loadershim.js
+++ gatsby/loadershim.js
@@ -2,6 +2,8 @@ global.___loader = {
   enqueue: jest.fn()
 };
 
+// Ensure tests fail for prop type errors.
+// https://medium.com/shark-bytes/type-checking-with-prop-types-in-jest-e0cd0dc92d5
 const originalConsoleError = console.error;
 console.error = message => {
   if (/(Failed prop type)/.test(message)) {
